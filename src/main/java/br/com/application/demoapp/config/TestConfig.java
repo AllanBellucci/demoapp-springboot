b514450dbc7904933521ed881a17f79502d5498f
AllanBellucci <allanbellucci@gmail.com>
2020-10-11 17:30:31 -0300

    OrdeStatus created

--- src/main/java/br/com/application/demoapp/config/TestConfig.java
+++ src/main/java/br/com/application/demoapp/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.application.demoapp.entities.Order;
 import br.com.application.demoapp.entities.User;
+import br.com.application.demoapp.entities.enuns.OrderStatus;
 import br.com.application.demoapp.repositories.OrderRepository;
 import br.com.application.demoapp.repositories.UserRepository;
 
@@ -29,8 +30,8 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Allan", "9999-9999", "email@email.com", "123456", null);
 		User user2 = new User(null, "Joao", "8888-8888", "email1@email.com", "123456", null);
 
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, user1);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.SHIPPED, user2);
 	
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1,order2));
