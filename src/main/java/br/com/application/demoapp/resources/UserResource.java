16a2a644d520800f827f3367f735ba37a5a86ff9
AllanBellucci <allanbellucci@gmail.com>
2020-10-12 21:03:21 -0300

    create Exceptions

--- src/main/java/br/com/application/demoapp/resources/UserResource.java
+++ src/main/java/br/com/application/demoapp/resources/UserResource.java
@@ -1,13 +1,19 @@
 package br.com.application.demoapp.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.application.demoapp.entities.User;
 import br.com.application.demoapp.services.UserServices;
@@ -18,17 +24,37 @@ public class UserResource {
 
 	@Autowired
 	private UserServices services;
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = services.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User user = services.findById(id);
-		return ResponseEntity.ok().body(user);
-		
+		User obj = services.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = services.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		services.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User obj){
+		obj = services.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 }
